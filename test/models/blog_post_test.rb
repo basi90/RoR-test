92dd7228794a5870d55980dcdf7d86e2e69d47d5
Andrea Basili <andrea.basili@live.it>
2025-10-30 22:53:32 +0100

    testing status and methods of BlogPost class

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "draft? returns true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for posted blog post" do
+    refute blog_posts(:posted).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "posted? returns false for draft blog post" do
+    refute blog_posts(:draft).posted?
+  end
+
+  test "posted? returns true for posted blog post" do
+    assert blog_posts(:posted).posted?
+  end
+
+  test "posted? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).posted?
+  end
+
+  test "scheduled? returns false for draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns true for posted blog post" do
+    refute blog_posts(:posted).scheduled?
+  end
+
+  test "scheduled? returns false for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  # def draft_blog_post
+  #   BlogPost.new(posted_at: nil)
+  # end
+
+  # def posted_blog_post
+  #   BlogPost.new(posted_at: 1.year.ago)
+  # end
+
+  # def scheduled_blog_post
+  #   BlogPost.new(posted_at: 1.year.from_now)
   # end
 end
