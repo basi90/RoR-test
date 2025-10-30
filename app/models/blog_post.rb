c8a27c5666f4d6c280111dcf11bdefbea35b17f2
Andrea Basili <andrea.basili@live.it>
2025-10-30 03:51:08 +0100

    remove fetch in database.yml due to conflict with rail server command. generate migration for posted_at field, added scope and methods to BlogPost class, and edit views accordingly

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,21 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :sorted, -> { order(posted_at: :desc, updated_at: :desc) }
+  scope :draft, -> { where(posted_at: nill) }
+  scope :posted, -> { where("posted_at <= ?", Time.current) }
+  scope :scheduled, -> { where("posted_at >= ?", Time.current) }
+
+  def draft?
+    posted_at.nil?
+  end
+
+  def posted?
+    posted_at? && posted_at <= Time.current
+  end
+
+  def scheduled?
+    posted_at? && posted_at > Time.current
+  end
 end
